68968edd7898b7d466189bba50a579141b5ed9b3
Daniela Erdmann <danielastanke@gmx.net>
2021-11-30 07:26:31 -0800

    refactor: Remove washington.csv from bikeshare_2.py

--- bikeshare_2.py
+++ bikeshare_2.py
@@ -3,8 +3,7 @@ import pandas as pd
 import numpy as np
 
 CITY_DATA = { 'chicago': 'chicago.csv',
-              'new york city': 'new_york_city.csv',
-              'washington': 'washington.csv' }
+              'new york city': 'new_york_city.csv'}
 
 def get_filters():
     """
